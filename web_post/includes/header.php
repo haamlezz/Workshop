2e4696dd994b21ae13d334bc53a2c3162488ae63
haamlezz <vongthavone@gmail.com>
2015-11-28 10:22:18 +0700

    Change
    Something change

---
+++ web_post/includes/header.php
@@ -0,0 +1,12 @@
+<?php
+    include_once 'dbconfig.php';
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title ?></title>
+        <link href="style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        
---
+++ web_post/index.php
@@ -0,0 +1,14 @@
+
+<?php 
+$title = 'ໜ້າຫຼັກ';
+require_once 'includes/header.php';
+?>
+
+<h1>Welcome to Web Post</h1>
+
+<div class="menu">
+    <a href="new_post.php">ສ້າງຫົວຂໍ້ໃໝ່</a>
+    <a href="new_post.php">ສ້າງຫົວຂໍ້ໃໝ່</a>
+</div>
+
+<?php require_once 'includes/footer.php';?>
